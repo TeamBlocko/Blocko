b038353b081c603ac9d4bbe2e5b8b86b9e074b38
alihsaas <alihsaas2@gmail.com>
2021-01-16 21:48:54 +0200

    add brightness functionality and change to 0-1

--- src/client/components/WorldMenuFrames/SettingsFrame/Lighting.tsx
+++ src/client/components/WorldMenuFrames/SettingsFrame/Lighting.tsx
@@ -1,10 +1,19 @@
 import Roact from "@rbxts/roact";
+import { connect } from "@rbxts/roact-rodux";
 import Catagory from "./Catagory";
+import { updateWorldSettings } from "shared/worldSettingsReducer"
 import ColorDisplay from "client/components/GraphicalWidget/ColorDisplay";
 import Slider from "client/components/GraphicalWidget/Slider";
 import SliderAndCheckBox from "client/components/GraphicalWidget/SliderAndCheckbox";
 
-function Lighting() {
+interface LightingPropTypes extends WorldSettings {
+	OnSliderUpdate(propertyName: string, value: number): void;
+	// OnDropdownUpdate(propertyName: string, value: Instance | Enum.Material): void;
+	// OnCheckBoxUpdate(propertyName: string, value: boolean): void;
+	// OnColorPickerUpdate(propertyName: string, value: Color3): void;
+}
+
+function Lighting(props: LightingPropTypes) {
 	return (
 		<frame BackgroundTransparency={1} Size={new UDim2(0.95, 0, 0, 305)}>
 			<uicorner CornerRadius={new UDim(0.05, 0)} />
@@ -12,7 +21,7 @@ function Lighting() {
 			<ColorDisplay Name="Ambient" Default={new Color3()} OnChange={(newValue) => print(newValue)} />
 			<ColorDisplay Name="Outdoor Ambient" Default={new Color3()} OnChange={(newValue) => print(newValue)} />
 			<Slider Name="Time" Min={0} Max={12} Default={0} OnChange={(newValue) => print(newValue)} />
-			<Slider Name="Brightness" Min={0} Max={10} Default={0} OnChange={(newValue) => print(newValue)} />
+			<Slider Name="Brightness" Min={0} Max={1} Default={props.Brightness} OnChange={(newValue) => props.OnSliderUpdate("Brightness", newValue)} />
 			<SliderAndCheckBox
 				Name="Cycle"
 				SliderSettings={{ Min: 0, Max: 10, Default: 5, OnChange: (newValue) => print(newValue) }}
@@ -23,4 +32,17 @@ function Lighting() {
 	);
 }
 
-export default Lighting;
+export default connect(
+	(state: IState) => state.WorldSettings,
+	(dispatch) => ({
+		OnSliderUpdate(propertyName: "Brightness", value: number) {
+			dispatch(
+				updateWorldSettings([{
+						propertyName,
+						value,
+					}]
+				)
+			)
+		}
+	})
+)(Lighting);
