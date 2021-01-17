041ac4565ce4dabeb6af2b465838e2d77ddd7dc8
alihsaas <alihsaas2@gmail.com>
2021-01-17 21:59:32 +0200

    connect Sound to rodux

--- src/client/components/WorldMenuFrames/SettingsFrame/Sound.tsx
+++ src/client/components/WorldMenuFrames/SettingsFrame/Sound.tsx
@@ -1,33 +1,78 @@
 import Roact from "@rbxts/roact";
+import { connect } from "@rbxts/roact-rodux";
+import { updateWorldSettings } from "shared/worldSettingsReducer";
 import Catagory from "./Catagory";
 import Slider from "client/components/GraphicalWidget/Slider";
 import InputFrame from "client/components/GraphicalWidget/InputFrame";
 
-function Sound() {
+interface SoundPropTypes extends WorldSettings {
+	OnSliderUpdate(propertyName: "Volume" | "Pitch", value: number): void;
+	OnCheckBoxUpdate(propertyName: "IsPlaying", value: boolean): void;
+	OnInputBoxUpdate(propertyName: "SoundID", value: string): void;
+}
+
+function Sound(props: SoundPropTypes) {
 	return (
 		<frame BackgroundTransparency={1} Size={new UDim2(0.95, 0, 0, 225)}>
 			<uicorner CornerRadius={new UDim(0.05, 0)} />
 			<Catagory Text="Sound" Image="rbxassetid://3926307971" />
-			<InputFrame Name="Audio ID" Length={40}>
+			<InputFrame Name="Sound Id" Length={40} OnChange={(newValue) => props.OnInputBoxUpdate("SoundID", newValue)}>
 				<frame Size={UDim2.fromScale(1, 0)}>
 					<imagebutton
 						AnchorPoint={new Vector2(0.5, 0.5)}
 						BackgroundTransparency={1}
 						Position={new UDim2(0.925, 0, 0.25, 10)}
 						Size={UDim2.fromOffset(20, 20)}
-						Image="rbxassetid://3926307971"
-						ImageColor3={Color3.fromRGB(199, 199, 199)}
-						ImageRectOffset={new Vector2(764, 244)}
+						Image={!props.IsPlaying ? "rbxassetid://3926307971" : "rbxassetid://3926307971"}
+						ImageColor3={!props.IsPlaying ? Color3.fromRGB(199, 199, 199) : Color3.fromRGB(84, 169, 199)}
+						ImageRectOffset={!props.IsPlaying ? new Vector2(764, 244) : new Vector2(804, 124)}
 						ImageRectSize={new Vector2(36, 36)}
 						ScaleType={Enum.ScaleType.Fit}
+						Event={{
+							Activated: () => props.OnCheckBoxUpdate("IsPlaying", !props.IsPlaying)
+						}}
 					/>
 				</frame>
 			</InputFrame>
-			<Slider Name="Volume" Min={0} Max={10} Default={3} OnChange={(newValue) => print(newValue)} />
-			<Slider Name="Pitch" Min={0} Max={100} Default={0} OnChange={(newValue) => print(newValue)} />
+			<Slider Name="Volume" Min={0} Max={10} Default={props.Volume} OnChange={(newValue) => props.OnSliderUpdate("Volume", newValue)} />
+			<Slider Name="Pitch" Min={0} Max={100} Default={props.Pitch} OnChange={(newValue) => props.OnSliderUpdate("Pitch", newValue)} />
 			<uilistlayout HorizontalAlignment={Enum.HorizontalAlignment.Center} Padding={new UDim(0, 10)} />
 		</frame>
 	);
 }
 
-export default Sound;
+export default connect(
+	(state: IState) => state.WorldSettings,
+	(dispatch) => ({
+		OnSliderUpdate(propertyName: "Volume" | "Pitch", value: number) {
+			dispatch(
+				updateWorldSettings([
+					{
+						propertyName,
+						value,
+					},
+				]),
+			);
+		},
+		OnCheckBoxUpdate(propertyName: "IsPlaying", value: boolean) {
+			dispatch(
+				updateWorldSettings([
+					{
+						propertyName,
+						value,
+					},
+				]),
+			);
+		},
+		OnInputBoxUpdate(propertyName: "SoundID", value: string) {
+			dispatch(
+				updateWorldSettings([
+					{
+						propertyName,
+						value,
+					},
+				]),
+			);
+		}
+	}),
+)(Sound);
