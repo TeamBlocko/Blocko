6f353d237dd6037ea2be3ad115e97b194c8583ce
alihsaas <alihsaas2@gmail.com>
2021-01-18 22:52:01 +0200

    update Characters settings

--- src/client/components/WorldMenuFrames/SettingsFrame/Characters.tsx
+++ src/client/components/WorldMenuFrames/SettingsFrame/Characters.tsx
@@ -1,41 +1,68 @@
 import Roact from "@rbxts/roact";
+import { connect } from "@rbxts/roact-rodux";
 import Catagory from "./Catagory";
+import { updateWorldSettings } from "shared/worldSettingsReducer";
 import Slider from "client/components/GraphicalWidget/Slider";
 import CheckBox from "client/components/GraphicalWidget/CheckBox";
 
-function Characters() {
+interface CharactersPropTypes extends WorldSettings {
+	OnSliderInputUpdate(propertyName: "UsernameDistance" | "HealthDistance" | "DefaultWalkSpeed" | "DefaultJumpPower" | "MinCameraZoom" | "MaxCameraZoom", newValue: number): void
+	OnCheckBoxUpdate(propertyName: "ResetEnabled" | "CollisionsEnabled", newValue: boolean): void
+}
+
+function Characters(props: CharactersPropTypes) {
 	return (
 		<frame BackgroundTransparency={1} Size={new UDim2(0.95, 0, 0, 535)}>
 			<uicorner CornerRadius={new UDim(0.05, 0)} />
 			<Catagory Text="Characters" Image="rbxassetid://3926305904" />
-			<CheckBox Name="Reset Allowed" Default={true} OnChange={(newValue) => print(newValue)} />
-			<CheckBox Name="Collision" Default={true} OnChange={(newValue) => print(newValue)} />
+			<CheckBox Name="Reset Allowed" Default={props.ResetEnabled} OnChange={(newValue) => props.OnCheckBoxUpdate("ResetEnabled", newValue)} />
+			<CheckBox Name="Collision" Default={true} OnChange={(newValue) => props.OnCheckBoxUpdate("CollisionsEnabled", newValue)} />
 			<Slider
 				Name="Username View Distance"
 				Min={0}
 				Max={100}
-				Default={50}
-				OnChange={(newValue) => print(newValue)}
+				Default={props.UsernameDistance}
+				OnChange={(newValue) => props.OnSliderInputUpdate("UsernameDistance", newValue)}
 			/>
 			<Slider
 				Name="Health View Distance"
 				Min={0}
 				Max={100}
-				Default={50}
-				OnChange={(newValue) => print(newValue)}
+				Default={props.HealthDistance}
+				OnChange={(newValue) => props.OnSliderInputUpdate("HealthDistance", newValue)}
 			/>
-			<Slider Name="Walk Speed" Min={0} Max={100} Default={16} OnChange={(newValue) => print(newValue)} />
-			<Slider Name="Minimum Camera Zoom" Min={0} Max={400} Default={0} OnChange={(newValue) => print(newValue)} />
+			<Slider Name="Walk Speed" Min={0} Max={100} Default={props.DefaultWalkSpeed} OnChange={(newValue) => props.OnSliderInputUpdate("DefaultWalkSpeed", newValue)} />
+			<Slider Name="Minimum Camera Zoom" Min={0} Max={400} Default={props.MinCameraZoom} OnChange={(newValue) => props.OnSliderInputUpdate("MinCameraZoom", newValue)} />
 			<Slider
 				Name="Maximum Camera Zoom"
 				Min={0}
 				Max={400}
-				Default={50}
-				OnChange={(newValue) => print(newValue)}
+				Default={props.MaxCameraZoom}
+				OnChange={(newValue) => props.OnSliderInputUpdate("MaxCameraZoom", newValue)}
 			/>
 			<uilistlayout HorizontalAlignment={Enum.HorizontalAlignment.Center} Padding={new UDim(0, 10)} />
 		</frame>
 	);
 }
 
-export default Characters;
+export default connect(
+	(state: IState) => state.WorldSettings,
+	(dispatch) => ({
+		OnSliderInputUpdate(propertyName: "UsernameDistance" | "HealthDistance" | "DefaultWalkSpeed" | "DefaultJumpPower" | "MinCameraZoom" | "MaxCameraZoom", value: number) {
+			dispatch(
+				updateWorldSettings([{
+					propertyName,
+					value,
+				}])
+			)
+		},
+		OnCheckBoxUpdate(propertyName: "ResetEnabled" | "CollisionsEnabled", value: boolean) {
+			dispatch(
+				updateWorldSettings([{
+					propertyName,
+					value,
+				}])
+			)
+		},
+	})
+)(Characters);
