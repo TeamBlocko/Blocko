68c7ac0ce3681f280e0410474ce059e5ca321158
alihsaas <alihsaas2@gmail.com>
2021-03-13 10:34:28 +0200

    update apply prompt

--- src/client/components/ApplyPrompt.tsx
+++ src/client/components/ApplyPrompt.tsx
@@ -17,10 +17,10 @@ export class ApplyPrompt extends Notification {
 			<frame
 				AnchorPoint={new Vector2(0.5, 0)}
 				BackgroundColor3={new Color3()}
-				BackgroundTransparency={this.binding.map((value) => map(value, 0, 1, 0.5, 1))}
-				Position={this.binding2.map((value) => {
+				BackgroundTransparency={this.binding.map((value) => map(value.Transparency, 0, 1, 0.5, 1))}
+				Position={this.binding.map((value) => {
 					const frame = this.frameRef.getValue();
-					if (frame) return frame.Position.Lerp(this.props.Position, value);
+					if (frame) return frame.Position.Lerp(this.props.Position, value.Position);
 					return this.props.Position;
 				})}
 				Size={UDim2.fromOffset(550, 50)}
@@ -34,7 +34,7 @@ export class ApplyPrompt extends Notification {
 					Size={UDim2.fromOffset(350, 20)}
 					Font={Enum.Font.GothamSemibold}
 					Text="You have unapplied changed! "
-					TextTransparency={this.binding.map((value) => value)}
+					TextTransparency={this.binding.map((value) => value.Transparency)}
 					TextColor3={new Color3(1, 1, 1)}
 					TextSize={18}
 					TextWrapped={true}
@@ -52,7 +52,7 @@ export class ApplyPrompt extends Notification {
 						Size={UDim2.fromOffset(65, 20)}
 						Font={Enum.Font.Gotham}
 						Text="Cancel"
-						TextTransparency={this.binding.map((value) => value)}
+						TextTransparency={this.binding.map((value) => value.Transparency)}
 						TextColor3={Color3.fromRGB(214, 214, 214)}
 						TextSize={18}
 						TextWrapped={true}
@@ -65,7 +65,7 @@ export class ApplyPrompt extends Notification {
 						Size={UDim2.fromOffset(65, 20)}
 						Font={Enum.Font.Gotham}
 						Text="Apply"
-						TextTransparency={this.binding.map((value) => value)}
+						TextTransparency={this.binding.map((value) => value.Transparency)}
 						TextColor3={Color3.fromRGB(66, 183, 255)}
 						TextSize={18}
 						TextWrapped={true}
@@ -89,7 +89,7 @@ export class ApplyPrompt extends Notification {
 					Image="rbxassetid://3926305904"
 					ImageRectOffset={new Vector2(364, 324)}
 					ImageRectSize={new Vector2(36, 36)}
-					ImageTransparency={this.binding.map((value) => value)}
+					ImageTransparency={this.binding.map((value) => value.Transparency)}
 				/>
 			</frame>
 		);
