50a1d22d65c2da71f4ebd6bd041d7693782c66fb
alihsaas <alihsaas2@gmail.com>
2021-03-13 10:36:53 +0200

    update types

---
+++ src/shared/Types.ts
@@ -0,0 +1,13 @@
+import { FunctionalitiesInstances } from "shared/Functionalities"
+
+export interface IState {
+	PlacementSettings: PlacementSettings;
+	WorldInfo: WorldInfo;
+}
+
+export interface PlacementSettings {
+	Shape: BasePart;
+	BuildMode: BuildMode;
+	RawProperties: RawProperties;
+	Functionalities: FunctionalitiesInstances[];
+}
