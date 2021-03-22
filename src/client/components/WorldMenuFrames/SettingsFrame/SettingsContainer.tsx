673bc15614364529c2a1049b5bd9b58d32cbb499
alihsaas <alihsaas2@gmail.com>
2021-03-22 13:54:41 +0200

    fix World Settings

--- src/client/components/WorldMenuFrames/SettingsFrame/SettingsContainer.tsx
+++ src/client/components/WorldMenuFrames/SettingsFrame/SettingsContainer.tsx
@@ -6,12 +6,15 @@ function WorldMenuFramesContainer(props: PropTypes) {
 			<uilistlayout
 				HorizontalAlignment={Enum.HorizontalAlignment.Center}
 				Padding={new UDim(0, 5)}
-				Ref={(e) =>
-					e.AncestryChanged.Connect(() => {
-						const parent = e.Parent as Frame;
-						const contentSize = e.AbsoluteContentSize;
-						parent.Size = new UDim2(0.95, 0, 0, contentSize.Y);
-					})
+				Ref={(e) => {
+						if (e === undefined) return;
+						e.AncestryChanged.Connect(() => {
+							const parent = e.Parent as Frame;
+							if (parent === undefined) return;
+							const contentSize = e.AbsoluteContentSize;
+							parent.Size = new UDim2(0.95, 0, 0, contentSize.Y);
+						})
+					}
 				}
 			/>
 			<Roact.Fragment>{props[Roact.Children]}</Roact.Fragment>
