464ecd44002ca688b86a26faa5de062ac2364b20
alihsaas <alihsaas2@gmail.com>
2021-04-04 18:28:05 +0300

    add moving scroll position to first element with key pressed name- dropdown list

--- src/client/components/GraphicalWidget/Dropdown/ItemList.tsx
+++ src/client/components/GraphicalWidget/Dropdown/ItemList.tsx
@@ -1,4 +1,5 @@
-import Roact, { RoactBinding } from "@rbxts/roact";
+import { UserInputService } from "@rbxts/services";
+import Roact, { RoactBinding, createRef } from "@rbxts/roact";
 
 function ItemElement<T extends Item>(props: { Value: T; Handler: NonNullable<RoactEvents<TextButton>["Activated"]> }) {
 	return (
@@ -28,39 +29,69 @@ export interface ItemListPropTypes<T> {
 	Binding: RoactBinding<number>;
 }
 
-function ItemList<T extends Item>(props: ItemListPropTypes<T>) {
-	return (
-		<frame
-			AnchorPoint={new Vector2(1, 0)}
-			BackgroundColor3={Color3.fromRGB(60, 60, 60)}
-			BorderSizePixel={0}
-			Position={new UDim2(1, 0, 1, 5)}
-			Size={props.Binding.map((value) => UDim2.fromOffset(135, 150 * value))}
-			ZIndex={10}
-		>
-			<uicorner CornerRadius={new UDim(0, 5)} />
-			<scrollingframe
-				AnchorPoint={new Vector2(0.5, 0.5)}
-				BackgroundColor3={new Color3(1, 1, 1)}
-				BackgroundTransparency={1}
+class ItemList<T extends Item> extends Roact.Component<ItemListPropTypes<T>> {
+	
+	itemsListRef: Roact.Ref<ScrollingFrame>;
+	connection: RBXScriptConnection | undefined;
+
+	constructor(props: ItemListPropTypes<T>) {
+		super(props);
+
+		this.itemsListRef = createRef();
+
+		props.Items.sort((a, b) => a.Name < b.Name)
+	}
+
+	render() {
+		return (
+			<frame
+				AnchorPoint={new Vector2(1, 0)}
+				BackgroundColor3={Color3.fromRGB(60, 60, 60)}
 				BorderSizePixel={0}
-				Position={UDim2.fromScale(0.5, 0.5)}
-				Size={UDim2.fromScale(0.96, 1)}
-				AutomaticCanvasSize={Enum.AutomaticSize.Y}
-				ScrollingDirection={Enum.ScrollingDirection.Y}
-				CanvasSize={UDim2.fromOffset(125, 18 * props.Items.size())}
-				ScrollBarImageColor3={Color3.fromRGB(31, 31, 31)}
-				ScrollBarImageTransparency={props.Binding.map((value) => 1 - value)}
-				ScrollBarThickness={5}
-				VerticalScrollBarInset={Enum.ScrollBarInset.ScrollBar}
+				Position={new UDim2(1, 0, 1, 5)}
+				Size={this.props.Binding.map((value) => UDim2.fromOffset(135, 150 * value))}
+				ZIndex={10}
 			>
-				<uilistlayout HorizontalAlignment={Enum.HorizontalAlignment.Center} />
-				{props.Items.map((Item) => (
-					<ItemElement Value={Item} Handler={props.OnSelected} />
-				))}
-			</scrollingframe>
-		</frame>
-	);
+				<uicorner CornerRadius={new UDim(0, 5)} />
+				<scrollingframe
+					Ref={this.itemsListRef}
+					AnchorPoint={new Vector2(0.5, 0.5)}
+					BackgroundColor3={new Color3(1, 1, 1)}
+					BackgroundTransparency={1}
+					BorderSizePixel={0}
+					Position={UDim2.fromScale(0.5, 0.5)}
+					Size={UDim2.fromScale(0.96, 1)}
+					AutomaticCanvasSize={Enum.AutomaticSize.Y}
+					ScrollingDirection={Enum.ScrollingDirection.Y}
+					CanvasSize={UDim2.fromOffset(125, 18 * this.props.Items.size())}
+					ScrollBarImageColor3={Color3.fromRGB(31, 31, 31)}
+					ScrollBarImageTransparency={this.props.Binding.map((value) => 1 - value)}
+					ScrollBarThickness={5}
+					VerticalScrollBarInset={Enum.ScrollBarInset.ScrollBar}
+				>
+					<uilistlayout HorizontalAlignment={Enum.HorizontalAlignment.Center} />
+					{this.props.Items.map((Item) => (
+						<ItemElement Value={Item} Handler={this.props.OnSelected} />
+					))}
+				</scrollingframe>
+			</frame>
+		);
+	}
+
+	didMount() {
+		const scrollingFrame = this.itemsListRef.getValue()
+		if (!scrollingFrame) return;
+
+		if (this.connection) this.connection.Disconnect();
+		this.connection = UserInputService.InputBegan.Connect((inputObject, gameProcessed) => {
+			if (gameProcessed) return
+			if (inputObject.UserInputType === Enum.UserInputType.Keyboard && inputObject.KeyCode.Name.size() === 0) {
+				const objectIndex = this.props.Items.findIndex((item) => item.Name.sub(1, 1) === inputObject.KeyCode.Name)
+				if (objectIndex === -1) return;
+				scrollingFrame.CanvasPosition = new Vector2(0, 18 * (objectIndex))
+			}
+		})
+	}
 }
 
 export default ItemList;
