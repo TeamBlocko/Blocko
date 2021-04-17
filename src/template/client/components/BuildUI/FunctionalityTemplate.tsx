9960a249826bf2bc12019c132da1bc77b88cb0d6
alihsaas <alihsaas2@gmail.com>
2021-04-18 00:50:24 +0300

    update FuctionalityTemplate

--- src/template/client/components/BuildUI/FunctionalityTemplate.tsx
+++ src/template/client/components/BuildUI/FunctionalityTemplate.tsx
@@ -28,53 +28,9 @@ interface FunctionTemplatePropTypes extends PlacementSettings {
 	RemoveFunctionality(guid: string): void;
 }
 
-function FunctionTemplate(props: FunctionTemplatePropTypes) {
+function renderFunctionalitySettings(props: FunctionTemplatePropTypes) {
 	return (
-		<frame
-			Key={props.Functionality.GUID}
-			BackgroundColor3={new Color3(1, 1, 1)}
-			BackgroundTransparency={0.95}
-			Size={new UDim2(0.975, 0, 0, 25)}
-			ZIndex={props.ZIndex}
-			LayoutOrder={props.LayoutOrder}
-		>
-			<uicorner CornerRadius={new UDim(0, 7)} />
-			<frame
-				AnchorPoint={new Vector2(0.5, 0)}
-				BackgroundTransparency={1}
-				BorderColor3={new Color3()}
-				BorderSizePixel={0}
-				ZIndex={11}
-				Position={UDim2.fromScale(0.5, 0.025)}
-				Size={new UDim2(1, 0, 0, 25)}
-			>
-				<Dropdown
-					Name="Functionality"
-					Items={getAvliableFunctionalities()}
-					GetValue={(value) =>
-						values(Functionalities.functionalities).find((functionality) => functionality.Name === value) ??
-						props.Functionality
-					}
-					OverrideValueText="Select Functionality"
-					ZIndex={11}
-					ResizeButtonToText={true}
-					Default={props.Functionality as Functionalities.FunctionalitiesValues}
-					OnChange={(value) => props.UpdateFunctionality(props.Functionality.GUID, value)}
-				/>
-				<Gap Width={40} />
-				<CloseButton OnClose={() => props.RemoveFunctionality(props.Functionality.GUID)} />
-				<uilistlayout
-					FillDirection={Enum.FillDirection.Horizontal}
-					HorizontalAlignment={Enum.HorizontalAlignment.Center}
-					VerticalAlignment={Enum.VerticalAlignment.Center}
-				/>
-			</frame>
-			<TitleText
-				Text={props.Functionality.Name}
-				TextXAlignment={Enum.TextXAlignment.Center}
-				Size={UDim2.fromOffset(180, 14)}
-			/>
-			{(values(props.Functionality.Properties) as Functionalities.FunctionalitiesPropertiesInstance[]).map(
+		(values(props.Functionality.Properties) as Functionalities.FunctionalitiesPropertiesInstance[]).map(
 				(property) => {
 					switch (property.Type) {
 						case "number":
@@ -95,21 +51,87 @@ function FunctionTemplate(props: FunctionTemplatePropTypes) {
 							);
 					}
 				},
-			)}
-			<uilistlayout
-				HorizontalAlignment={Enum.HorizontalAlignment.Center}
-				Ref={(e) => {
-					if (!e) return;
+			)
+	)
+};
 
-					const parent = e.Parent as Frame;
-					parent.Size = new UDim2(0.975, 0, 0, e.AbsoluteContentSize.Y);
-					e.GetPropertyChangedSignal("AbsoluteContentSize").Connect(() => {
-						parent.Size = new UDim2(0.975, 0, 0, e.AbsoluteContentSize.Y);
-					});
-				}}
-			/>
-		</frame>
-	);
+class FunctionTemplate extends Roact.Component<FunctionTemplatePropTypes> {
+
+	uilistRef: Roact.Ref<UIListLayout>
+
+	constructor(props: FunctionTemplatePropTypes) {
+		super(props)
+
+		this.uilistRef = Roact.createRef()
+	}
+
+	render() {
+		const settings = renderFunctionalitySettings(this.props)
+		return (
+			<frame
+				Key={this.props.Functionality.GUID}
+				BackgroundColor3={new Color3(1, 1, 1)}
+				BackgroundTransparency={0.95}
+				Size={new UDim2(0.975, 0, 0, 35)}
+				ZIndex={this.props.ZIndex}
+				LayoutOrder={this.props.LayoutOrder}
+			>
+				<uicorner CornerRadius={new UDim(0, 7)} />
+				<frame
+					AnchorPoint={new Vector2(0.5, 0)}
+					BackgroundTransparency={1}
+					BorderColor3={new Color3()}
+					BorderSizePixel={0}
+					ZIndex={11}
+					Position={UDim2.fromScale(0.5, 0.025)}
+					Size={new UDim2(1, 0, 0, 35)}
+				>
+					<Dropdown
+						Name="Functionality"
+						Items={getAvliableFunctionalities()}
+						GetValue={(value) =>
+							values(Functionalities.functionalities).find((functionality) => functionality.Name === value) ??
+							this.props.Functionality
+						}
+						ZIndex={11}
+						Default={this.props.Functionality as Functionalities.FunctionalitiesValues}
+						OnChange={(value) => this.props.UpdateFunctionality(this.props.Functionality.GUID, value)}
+					/>
+					<Gap Width={40} />
+					<CloseButton OnClose={() => this.props.RemoveFunctionality(this.props.Functionality.GUID)} />
+					<uilistlayout
+						FillDirection={Enum.FillDirection.Horizontal}
+						HorizontalAlignment={Enum.HorizontalAlignment.Center}
+						VerticalAlignment={Enum.VerticalAlignment.Center}
+					/>
+				</frame>
+				{ settings.size() > 0 ?
+					<TitleText
+						Text={"Functionality Settings"}
+						TextXAlignment={Enum.TextXAlignment.Center}
+						Size={UDim2.fromOffset(180, 14)}
+					/>
+					: undefined
+				}
+				{settings}
+				<uilistlayout
+					HorizontalAlignment={Enum.HorizontalAlignment.Center}
+					Ref={this.uilistRef}
+				/>
+			</frame>
+		);
+	}
+
+	didMount() {
+		const uilist = this.uilistRef.getValue();
+		if (!uilist) return;
+
+		const parent = uilist.Parent as Frame;
+		parent.Size = new UDim2(0.975, 0, 0, uilist.AbsoluteContentSize.Y);
+		uilist.GetPropertyChangedSignal("AbsoluteContentSize").Connect(() => {
+			parent.Size = new UDim2(0.975, 0, 0, uilist.AbsoluteContentSize.Y);
+		});
+	}
 }
 
 export default connect(
