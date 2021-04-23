4d10e92a5931017f43d19dfbea568c781f2a095d
alihsaas <alihsaas2@gmail.com>
2021-04-23 21:08:35 +0300

    add tweening to checkbox

--- src/template/client/components/GraphicalWidget/CheckBox/Check.tsx
+++ src/template/client/components/GraphicalWidget/CheckBox/Check.tsx
@@ -1,26 +1,46 @@
 import Roact from "@rbxts/roact";
+import Flipper from "@rbxts/flipper";
 
 interface Check {
 	Value: boolean;
 	HandleInput: () => void;
 }
 
-function CheckBoxElement(props: Check) {
-	return (
-		<imagebutton
-			AnchorPoint={new Vector2(0.5, 0.5)}
-			BackgroundTransparency={1}
-			Size={UDim2.fromOffset(20, 20)}
-			Visible={props.Value}
-			Event={{
-				Activated: () => props.HandleInput(),
-			}}
-			Position={UDim2.fromScale(0.5, 0.5)}
-			Image="rbxassetid://3926311105"
-			ImageRectOffset={new Vector2(4, 836)}
-			ImageRectSize={new Vector2(48, 48)}
-		/>
-	);
+class CheckBoxElement extends Roact.Component<Check> {
+
+	motor: Flipper.SingleMotor;
+	binding: Roact.RoactBinding<number>;
+	setBinding: Roact.RoactBindingFunc<number>;
+
+	constructor(props: Check) {
+		super(props);
+		this.motor = new Flipper.SingleMotor(props.Value ? 0 : 1);
+
+		[this.binding, this.setBinding] = Roact.createBinding(this.motor.getValue());
+
+		this.motor.onStep(this.setBinding);
+	}
+
+	render() {
+		return (
+			<imagebutton
+				AnchorPoint={new Vector2(0.5, 0.5)}
+				BackgroundTransparency={1}
+				Size={UDim2.fromOffset(20, 20)}
+				Event={{
+					Activated: () => {
+						this.motor.setGoal(new Flipper.Spring(!this.props.Value ? 0 : 1))
+						this.props.HandleInput()
+					},
+				}}
+				Position={UDim2.fromScale(0.5, 0.5)}
+				Image="rbxassetid://3926311105"
+				ImageTransparency={this.binding}
+				ImageRectOffset={new Vector2(4, 836)}
+				ImageRectSize={new Vector2(48, 48)}
+			/>
+		);
+	}
 }
 
 export default CheckBoxElement;
