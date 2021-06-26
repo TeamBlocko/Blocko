e237e889ef16a902567d7de931afc0948fc060fb
alihsaas <alihsaas2@gmail.com>
2021-06-26 11:56:00 +0300

    update functionality handling

--- src/template/server/placementHandler/FunctionalitiesHandler.ts
+++ src/template/server/placementHandler/FunctionalitiesHandler.ts
@@ -1,44 +1,64 @@
-import { RunService, CollectionService, Players } from "@rbxts/services";
+import { CollectionService, Players } from "@rbxts/services";
 import { values } from "@rbxts/object-utils";
 import * as Functionalities from "template/shared/Functionalities";
 
-function GetTouchingParts(part: BasePart) {
-	const connection = part.Touched.Connect(() => {});
-	const parts = part.GetTouchingParts();
-	connection.Disconnect();
-	return parts;
+const debounce = new Map<string, Map<Player, number>>();
+
+function addDamager(part: BasePart, functionality: Functionalities.FunctionalitiesInstancesValues) {
+	if (functionality.Name === "Damager") {
+		part.Touched.Connect((touchingPart) => {
+			const player = Players.GetPlayerFromCharacter(touchingPart.Parent);
+			const character = player?.Character;
+			const humanoid = character?.FindFirstChildOfClass("Humanoid");
+
+			if (!player || !humanoid || !character) return;
+			const time = debounce.get(functionality.GUID)!.get(player);
+
+			if (time === undefined || os.time() - time > functionality.Properties.Cooldown.Current) {
+				if (humanoid !== undefined) {
+					humanoid.TakeDamage(functionality.Properties.Damage.Current);
+
+					debounce.get(functionality.GUID)!.set(player, os.time());
+				}
+			}
+		});
+		CollectionService.AddTag(part, "Functionality");
+	}
 }
 
-function addDamager(part: BasePart, _properties: Functionalities.FunctionalitiesInstances["Damager"]["Properties"]) {
-	CollectionService.AddTag(part, "Functionality");
-}
-
-function addTripper(part: BasePart, _properties: Functionalities.FunctionalitiesInstances["Tripper"]["Properties"]) {
-	/*
+function addTripper(part: BasePart) {
 	part.Touched.Connect((object) => {
 		const humanoid = object.Parent!.FindFirstChildOfClass("Humanoid");
 		if (humanoid) {
 			humanoid.Sit = true;
 		}
 	});
-	*/
 	CollectionService.AddTag(part, "Functionality");
 }
 
-function addConveyor(part: BasePart, properties: Functionalities.FunctionalitiesInstances["Conveyor"]["Properties"]) {
-	CollectionService.AddTag(part, "Functionality");
+function addConveyor(part: BasePart, functionality: Functionalities.FunctionalitiesInstancesValues) {
+	if (functionality.Name === "Conveyor") {
+		part.Touched.Connect((otherPart) => {
+			const character = otherPart.Parent!;
+			const humanoidRootPart = character.FindFirstChild("HumanoidRootPart") as BasePart;
+			if (!humanoidRootPart) return;
+			part.AssemblyLinearVelocity = Vector3.FromNormalId(functionality.Properties.Direction.Current).mul(functionality.Properties.Speed.Current);
+				
+		});
+		CollectionService.AddTag(part, "Functionality");
+	}
 }
 
 export function addFunctionality(part: BasePart, functionality: Functionalities.FunctionalitiesInstancesValues) {
 	switch (functionality.Name) {
 		case "Damager":
-			addDamager(part, functionality.Properties);
+			addDamager(part, functionality);
 			break;
 		case "Tripper":
-			addTripper(part, functionality.Properties);
+			addTripper(part);
 			break;
 		case "Conveyor":
-			addConveyor(part, functionality.Properties);
+			addConveyor(part, functionality);
 			break;
 	}
 }
@@ -82,8 +102,6 @@ export function addPart(part: BasePart, functionalities: Functionalities.Functio
 	}
 }
 
-const debounce = new Map<Folder, Map<Player, number>>();
-
 export interface Damager {
 	Damage: NumberValue;
 	Cooldown: NumberValue;
@@ -93,40 +111,3 @@ export interface Conveyor {
 	Speed: NumberValue;
 	Direction: Vector3Value;
 }
-
-type Functionality = Folder &
-	(({ Name: "Damager" } & Damager) | ({ Name: "Conveyor" } & Conveyor) | { Name: "Tripper" });
-
-RunService.Heartbeat.Connect(() => {
-	for (const part of CollectionService.GetTagged("Functionality") as (BasePart & { Functionalities: Folder })[]) {
-		for (const functionality of part.Functionalities.GetChildren() as Functionality[]) {
-			debounce.get(functionality) ?? debounce.set(functionality, new Map());
-
-			for (const touchingPart of GetTouchingParts(part)) {
-				const player = Players.GetPlayerFromCharacter(touchingPart.Parent);
-				const character = player?.Character;
-				const humanoid = character?.FindFirstChildOfClass("Humanoid");
-
-				if (!player || !humanoid || !character) continue;
-
-				if (functionality.Name === "Damager") {
-					const time = debounce.get(functionality)!.get(player);
-
-					if (time === undefined || os.time() - time > functionality.Cooldown.Value) {
-						if (humanoid !== undefined) {
-							humanoid.TakeDamage(functionality.Damage.Value);
-
-							debounce.get(functionality)!.set(player, os.time());
-						}
-					}
-				} else if (functionality.Name === "Tripper") {
-					humanoid.Sit = true;
-				} else if (functionality.Name === "Conveyor") {
-					const humanoidRootPart = character.FindFirstChild("HumanoidRootPart") as BasePart;
-					if (!humanoidRootPart) continue;
-					part.AssemblyLinearVelocity = functionality.Direction.Value.mul(functionality.Speed.Value);
-				}
-			}
-		}
-	}
-});
