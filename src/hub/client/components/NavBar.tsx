c061f4db235451b296e9a7e1f49819a0e5017332
alihsaas <alihsaas2@gmail.com>
2021-07-29 12:03:37 +0300

    update sidebar

--- src/hub/client/components/NavBar.tsx
+++ src/hub/client/components/NavBar.tsx
@@ -14,26 +14,32 @@ function NavBar() {
 				Image="rbxassetid://3926305904"
 				ImageRectOffset={new Vector2(964, 204)}
 				ImageRectSize={new Vector2(36, 36)}
+				Selected={true}
+				OnClick={() => print("Home")}
 			/>
 			<NavButton
 				Image="rbxassetid://3926305904"
 				ImageRectOffset={new Vector2(200, 4)}
 				ImageRectSize={new Vector2(24, 24)}
+				OnClick={() => print("Shop")}
 			/>
 			<NavButton
 				Image="rbxassetid://3926305904"
 				ImageRectOffset={new Vector2(924, 364)}
 				ImageRectSize={new Vector2(36, 36)}
+				OnClick={() => print("Edit")}
 			/>
 			<NavButton
 				Image="rbxassetid://3926305904"
 				ImageRectOffset={new Vector2(144, 4)}
 				ImageRectSize={new Vector2(24, 24)}
+				OnClick={() => print("Friends")}
 			/>
 			<NavButton
 				Image="rbxassetid://3926305904"
 				ImageRectOffset={new Vector2(844, 564)}
 				ImageRectSize={new Vector2(36, 36)}
+				OnClick={() => print("Notifications")}
 			/>
 			<uilistlayout
 				HorizontalAlignment={Enum.HorizontalAlignment.Center}
--- src/hub/client/components/NavButton.tsx
+++ src/hub/client/components/NavButton.tsx
@@ -1,17 +1,73 @@
 import Roact from "@rbxts/roact";
+import Flipper from "@rbxts/flipper";
 
 interface NavButtonPropTypes {
 	Image: string;
 	ImageRectOffset: Vector2;
 	ImageRectSize: Vector2;
+	Selected?: boolean;
+	OnClick: () => void;
 }
 
-function NavButton(props: NavButtonPropTypes) {
-	return (
-		<imagebutton BackgroundTransparency={1} Size={UDim2.fromScale(0.3, 0.042)} {...props}>
-			<uiaspectratioconstraint AspectRatio={0.99516522884369} />
-		</imagebutton>
-	);
+type Goal = { hover: number; selected: number };
+
+class NavButton extends Roact.Component<NavButtonPropTypes> {
+	binding: Roact.RoactBinding<Goal>;
+	setBinding: Roact.RoactBindingFunc<Goal>;
+	motor: Flipper.GroupMotor<Goal>;
+
+	constructor(props: NavButtonPropTypes) {
+		super(props);
+
+		[this.binding, this.setBinding] = Roact.createBinding({ hover: 0, selected: this.props.Selected ? 1 : 0 });
+
+		this.motor = new Flipper.GroupMotor(this.binding.getValue());
+		this.motor.onStep(this.setBinding);
+	}
+
+	didUpdate() {
+		this.motor.setGoal({ selected: new Flipper.Spring(this.props.Selected ? 1 : 0) });
+	}
+
+	render() {
+		return (
+			<imagebutton
+				BackgroundColor3={new Color3(1, 1, 1)}
+				BackgroundTransparency={this.props.Selected ? 0.95 : 1}
+				Size={UDim2.fromScale(0.4, 0.4)}
+				AutoButtonColor={false}
+				BorderColor3={Color3.fromRGB(27, 42, 53)}
+				Event={{
+					Activated: () => this.props.OnClick(),
+					MouseEnter: () => this.motor.setGoal({ hover: new Flipper.Spring(1) }),
+					MouseLeave: () => this.motor.setGoal({ hover: new Flipper.Spring(0) }),
+				}}
+			>
+				<uiaspectratioconstraint />
+				<uicorner CornerRadius={this.binding.map((value) => new UDim(value.selected * 0.35, 0))} />
+				<imagelabel
+					Active={true}
+					AnchorPoint={new Vector2(0.5, 0.5)}
+					BackgroundTransparency={1}
+					BorderColor3={Color3.fromRGB(27, 42, 53)}
+					Position={UDim2.fromScale(0.5, 0.5)}
+					Selectable={true}
+					Size={UDim2.fromScale(0.6, 0.6)}
+					Image={this.props.Image}
+					ImageRectOffset={this.props.ImageRectOffset}
+					ImageRectSize={this.props.ImageRectSize}
+					ImageTransparency={this.binding.map((value) => (1 - value.selected) * 0.65)}
+					ImageColor3={
+						this.props.Selected
+							? Color3.fromRGB(69, 165, 255)
+							: this.binding.map((value) =>
+									new Color3(1, 1, 1).Lerp(Color3.fromRGB(158, 181, 255), value.hover),
+							  )
+					}
+				></imagelabel>
+			</imagebutton>
+		);
+	}
 }
 
 export default NavButton;
