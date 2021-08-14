7eec607841031c3caa18cd86ddb7c3453f4c71e0
alihsaas <alihsaas2@gmail.com>
2021-08-14 10:02:23 +0300

    update Lighting Settings

--- src/template/client/components/WorldMenuFrames/SettingsFrame/SettingsContainer.tsx
+++ src/template/client/components/WorldMenuFrames/SettingsFrame/SettingsContainer.tsx
@@ -1,10 +1,14 @@
 import Roact from "@rbxts/roact";
 
-class WorldMenuFramesContainer extends Roact.Component<Roact.PropsWithChildren> {
+interface WorldMenuFramesContainerPropTypes extends Roact.PropsWithChildren {
+	layoutProps?: Roact.JsxInstance<UIListLayout>;
+}
+
+class WorldMenuFramesContainer extends Roact.Component<WorldMenuFramesContainerPropTypes> {
 	binding: Roact.RoactBinding<number>;
 	setBinding: Roact.RoactBindingFunc<number>;
 
-	constructor(props: Roact.PropsWithChildren) {
+	constructor(props: WorldMenuFramesContainerPropTypes) {
 		super(props);
 
 		[this.binding, this.setBinding] = Roact.createBinding(0);
@@ -17,6 +21,7 @@ class WorldMenuFramesContainer extends Roact.Component<Roact.PropsWithChildren> 
 					HorizontalAlignment={Enum.HorizontalAlignment.Center}
 					Padding={new UDim(0, 5)}
 					Change={{ AbsoluteContentSize: (e) => this.setBinding(e.AbsoluteContentSize.Y) }}
+					{...this.props.layoutProps}
 				/>
 				<Roact.Fragment>{this.props[Roact.Children]}</Roact.Fragment>
 			</frame>
